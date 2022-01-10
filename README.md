ca33ec8066a5074f6d232c9b2973befc162f44e3
ShukujiNeel13 <neelshukuji@gmail.com>
2022-01-10 10:14:15 +0800

    Move app dev steps to Readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-Prototype in python for a basic microservice
+# Shukuji
 
-Based on Flask
-Currently uses SQLite as the database
+## Prototype in python for a basic microservice
 
+- Created using Flask
+- Currently uses SQLite as the database
+
+
+### App Dev Steps (After creation of respective tests, for TDD manner)
+1. Create Route for Index
+2. Database functions (steps)
+  2.1. Create the DB
+    2.1.1. Get the DB connection
+      2.1.1.1. Connect to DB
+    2.1.2. Save the DB connection for re-use by the application
+  2.2. Close the DB (connection) - when app needs to close.
+3. Create routes to perform desired operations.
+4. Update the index route to more specific version (if desired)
--- app.py
+++ app.py
@@ -27,16 +27,6 @@ APP.config.from_object(__name__)
 # TODO: Next step - Add SQLAlchemy to better manage db.
 # TODO: Add the HTML and JS to view the items on web.
 
-# App Dev Steps (After creation of respective tests, for TDD manner)
-# 1. Create Route for Index
-# 2. Database functions (steps)
-#   2.1. Create the DB
-#     2.1.1. Get the DB connection
-#       2.1.1.1. Connect to DB
-#     2.1.2. Save the DB connection for re-use by the application
-#   2.2. Close the DB (connection) - when app needs to close.
-# 3. Create routes to perform desired operations.
-# 4. Update the index route to more specific version (if desired)
 
 # TODO: How to check current number of connections to SQLite DB?
 
