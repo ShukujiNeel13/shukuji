41fd9bfcbc994073079e66df6653b849be392be2
ShukujiNeel13 <neelshukuji@gmail.com>
2022-01-10 13:15:53 +0800

    Capitalize SQL statements in script

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-drop table if exists entries;
-create table entries (
-    id integer primary key autoincrement,
-    title text not null,
-    text text not null
+DROP TABLE IF EXISTS entries;
+CREATE TABLE entries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title text NOT NULL,
+    text text NOT NULL
 );
